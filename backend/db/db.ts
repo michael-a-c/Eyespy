b09109decffaaf5764abf3af6eabf5bf55feffb0
michael-a-c <michael.cottow@mail.utoronto.ca>
2020-03-07 03:38:02 -0500

    clean up

--- backend/db/db.ts
+++
@@ -1,8 +0,0 @@
-const config = require("./local.settings.json")
-var MongoClient = require('mongodb').MongoClient
-
-MongoClient.connect(`mongodb://${config.mongoDbUsername}:${config.mongoDbUserPassword}@${config.mongoDbIp}:27017`, function (err, client) {
-  if(err) {console.error(`\nFAILED TO CONNECT TO DATABASE: ${err.message}`); process.exit(1)};
-
-  const db = client.db(config.mongoDbName)
-})
