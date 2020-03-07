801e47a736cf96445c2144ea43012a6e098ee6ca
michael-a-c <michael.cottow@mail.utoronto.ca>
2020-03-07 12:02:52 -0500

    Setup to integrate signup backend with frontend

--- backend/src/middleware.ts
+++ backend/src/middleware.ts
@@ -1,10 +1,9 @@
-import { Response, NextFunction } from "express";
-import { Request } from '../interfaces'
+import { Request, Response, NextFunction } from "express";
 
 import { UNAUTHORIZED, METHOD_FAILURE } from 'http-status-codes';
 
 let isAuthenticated = (req: Request, res: Response, next : NextFunction) => {
-    if (!req.session.user) return res.status(UNAUTHORIZED).json({ "message": "access denied" });
+    if (!req.session!.user) return res.status(UNAUTHORIZED).json({ "message": "access denied" });
     return next();
 }
 
