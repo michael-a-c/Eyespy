39961f67b78f8852e435c1f576f38766c4fc2ade
michael-a-c <michael.cottow@mail.utoronto.ca>
2020-04-09 17:24:43 -0300

    screenshots work in progress

--- src/controllers/ScreenshotController.ts
+++ src/controllers/ScreenshotController.ts
@@ -4,8 +4,10 @@ import { Logger } from '@overnightjs/logger';
 import { OK, BAD_REQUEST, INTERNAL_SERVER_ERROR, CONFLICT, NOT_FOUND, UNAUTHORIZED } from 'http-status-codes';
 import { CreateScreenshotRequest, CreateScreenshotRequestMaker} from '../Requests'
 import { Screenshot, UserSchema, IUser } from "../repository";
-import { NativeError } from 'mongoose';
+import { NativeError, Types} from 'mongoose';
 import { isAuthenticated } from '../middleware'
+import { stringify } from 'querystring';
+var path = require('path');
 
 const multer = require("multer");
 const upload = multer({ dest: './uploads/' });
@@ -33,19 +35,28 @@ export class ScreenshotController {
         if (!req.file) return res.status(400).end("BAD REQUEST: improperly formatted image");
         console.log(req.file);
         let newScreenshot = new Screenshot({username: "bing bong", title: createScreenshotRequest.title, path: req.file.path, mimetype: req.file.mimetype });
-        newScreenshot.save((err) => {
+        newScreenshot.save((err, result) => {
             if(err) {res.status(INTERNAL_SERVER_ERROR).end("Server Error")}
             else {
-                res.json(req.file);
+                res.json({id: result._id});
             }
         });
     }
 
-    @Get('view')
+    @Get('view/:id')
     //@Middleware(isAuthenticated)
     private view(req: Request, res: Response) {
         Logger.Info(req.url);
-       
+        let ObjectId = Types.ObjectId(req.params.id);
+        Screenshot.findById(ObjectId, (err,result) =>{
+            if(err) {
+                return res.status(INTERNAL_SERVER_ERROR).end();
+            }
+            if(!result){
+                return res.status(NOT_FOUND).end("image could not be found");
+            } 
+            res.sendFile(result.path, {'root': "."});
+        })
     }
 
     @Get('list')
